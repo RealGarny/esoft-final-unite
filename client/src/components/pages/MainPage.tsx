67aed34cedd5e89c6d0b79812d7b63ecbc328aa9
RealGarny <aseev.nik1@yandex.ru>
2024-05-14 16:52:29 +0500

    very small changes

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -4,6 +4,7 @@ const MainPage:React.FC = () => {
     return(
         <>
             <Splash/>
+            <div className="h-[600px] z-10 w-full bg-[color:var(--additional)]"></div>
         </>
     )
 }
